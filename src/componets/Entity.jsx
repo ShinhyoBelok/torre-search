fc88cfcac9fee4bf0fb4f739c2e5a0d826447ba8
ShinhyoBelok <shibo6937@gmail.com>
2023-09-08 14:23:53 -0400

    Add link to redirect to genome and add debounce to search request

--- src/componets/Entity.jsx
+++ src/componets/Entity.jsx
@@ -3,23 +3,34 @@ import { string } from 'prop-types';
 import { AiOutlineSave } from 'react-icons/ai';
 
 export default function Entity(props) {
-  const {name, professionalHeadline, ggId} = props;
+  const {
+    name,
+    professionalHeadline,
+    ggId,
+    username,
+    imageUrl
+  } = props;
 
   const saveEntity = (e) => {
+    e.preventDefault();
     console.log(e.target.id);
   }
 
   return (
-    <li>
-      <div>{name}</div>
-      <div>{professionalHeadline}</div>
-      <button id={ggId} onClick={saveEntity}>Save <AiOutlineSave /></button>
-    </li>
+    <a href={`https://torre.ai/${username}`} target="_blank">
+      <div>
+        <img src={imageUrl} alt="Profile Pic" />
+        <div>{name}</div>
+        <div>{professionalHeadline}</div>
+        <button id={ggId} onClick={saveEntity}>Save <AiOutlineSave /></button>
+      </div>
+    </a>
   )
 }
 
 Entity.propTypes = {
   name: string.isRequired,
   professionalHeadline: string.isRequired,
-  ggId: string.isRequired
+  ggId: string.isRequired,
+  username: string.isRequired
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,21 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getEntities, cleanState } from '../redux/entities/entities';
 import { v4 as uuidv4 } from 'uuid';
+import { debounce } from 'lodash'
 import Entity from '../componets/Entity';
 
 export default function Home() {
   const dispatch = useDispatch();
   const entities = useSelector((state) => state.entities);
 
+  // debounce to avoid multiple request when user is typing fast
+  const debouncedHandleChange = debounce((value) => {
+    console.log('request');
+    dispatch(getEntities(value));
+  }, 200);
+
   const handleChange = (e) => {
-    dispatch(getEntities(e.target.value));
+    debouncedHandleChange(e.target.value);
   }
 
   const hideEntities = () => {
@@ -26,6 +33,8 @@ export default function Home() {
             name={entity.name}
             professionalHeadline={entity.professionalHeadline}
             ggId={entity.ggId}
+            username={entity.username}
+            imageUrl={entity.imageUrl}
           />
         ))
       }
